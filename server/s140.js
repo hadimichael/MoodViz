6b8038f7a1cf2f2ddecd80a6ba7a0d67b0efc187
Hadi Michel Salem <hsalem@deloitte.com.au>
2013-06-27 18:06:58 +1000

    extract s140 into its own module

---
+++ server/s140.js
@@ -0,0 +1,47 @@
+/*
+ * @description: does cool stuff...
+ * @author: @hadi_michael
+ * @date: May 2013
+ *
+ */
+
+var app_creds 	= require('./app_credentials'), //see app_credentials__template.js
+	s140_appId 	= app_creds.getSentiment140AppId();
+
+/* Prepare data for Sentiment140 API */
+var s140_requestBuilder = {
+	url				: 'http://www.sentiment140.com/api/',
+	simpleClassify 	: 'classify',
+	bulkClassify 	: 'bulkClassifyJson',
+	appId 			: '?appid=' + s140_appId
+}
+
+exports.simple = function(tweet) {
+	try {
+		var text = "?text=" + tweet.text.replace(/\s/g,"+");
+		return s140_requestBuilder.url + s140_requestBuilder.simpleClassify + text;
+	} catch (err) {
+		console.error('Could not build simple request. Error: ' + err);
+		return null;
+	}		
+}
+
+exports.bulk = {
+	getUrl: function() {
+		return s140_requestBuilder.url + s140_requestBuilder.bulkClassify + s140_requestBuilder.appId;
+	},
+
+	getBody: function(tweets) {
+		try {
+			var data = [];
+			for (key in tweets.results) {
+				data.push({ "text": tweets.results[key].text });
+			}
+			return JSON.stringify({ "data": data });
+		} catch (err) {
+			console.error('Could not build bulk request. Error: ' + err);
+			return null;
+		}
+	},
+}
+/* /Prepare data for Sentiment140 API */
