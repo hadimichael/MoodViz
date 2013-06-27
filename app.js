49e1578dbaf9a7bb3bbe9d97cef69571f60fee21
Hadi Michel Salem <hsalem@deloitte.com.au>
2013-06-27 18:06:37 +1000

    define new routing system

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 /*
- * app.js -> node.js server for MoodViz
- *
- * Author: @hadi_michael
- * Date: May 2013
+ * @description: node.js server
+ * @author: @hadi_michael
+ * @date: May 2013
  *
  */
 
@@ -31,26 +30,42 @@ app.configure(function(){
 	}));
 });
 
+// start the server
 try {
 	server.listen(8080);
 	console.log('Server is listening on port: ' + 8080);
 } catch (err) {
-	console.error(err);
+	console.error('Server didn\'t start: ' + err);
 }
 /***** /SETUP AND START SERVER *****/
 
 /***** MANAGE SOCKETS *****/
-io.set('log level', 2); // reduce logging (Log levels: 0 - error / 1 - warn / 2 - info / 3 - debug)
+io.set('log level', 2); // set socket.io logging (Log levels: 0 - error / 1 - warn / 2 - info / 3 - debug)
 io.sockets.on('connection', function (socket) {
 
+	console.log('New user connected: ' + socket.id);
   	socket.emit('connected', { hello: 'Welcome to the party!' });
 
-  	socket.on('getTweets', function(data) {
-		require('./server/getTweets').getTweets(socket, data);
+  	/* define socket events */
+  	socket.on('tunein', function(something) {
+		//TODO: add this person the list of people listening to the stream...
+	});
+	
+	socket.on('stream.track', function(keywords) {
+		require('./server/stream').track(socket, keywords);
+	});
+	
+	socket.on('stream.follow', function(userIDs) {
+		require('./server/stream').follow(socket, userIDs);
+	});
+
+	socket.on('search', function(keywords) {
+		//COMING SOON!
+		//require('./server/search').search(keywords);
 	});
 
 	socket.on('disconnect', function () {
-    	console.log('user disconnected - I know... I also thought they liked us!');
+    	console.log('User disconnected - Damn... I thought they liked us!');
   	});
 
 });
