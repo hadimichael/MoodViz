e6dd97405e8fb065dca75039422661ffeb0f645f
Hadi Michel Salem <hsalem@deloitte.com.au>
2013-06-28 15:09:50 +1000

    update the creds template

--- server/app_credentials__template.js
+++ server/app_credentials__template.js
@@ -1,8 +1,7 @@
 /*
- * app_credentials__template.js -> getters for app credentials
  *
- * Author: @hadi_michael
- * Date: May 2013
+ * @author: @hadi_michael
+ * @date: May-July 2013
  *
  */
 
@@ -11,37 +10,19 @@
 //		3. Rename this file to: app_credentials.js
 
 exports.getOAuthSettings = function (appName) {
-	switch (appName) {
-		case 'MoodViz':
-			return { 
-		  		consumer_key: '', // TODO: YOUR CONSUMER KEY AS A STRING */
-			  	consumer_secret: '' // TODO: YOUR CONSUMER SECRET AS A STRING */
-		  	};
-		default:
-			throw new Error('No OAuth Settings were found for ' + appName);
-			return null;
-	};
+	return { 
+  		consumer_key: '', 	// TODO: YOUR CONSUMER KEY AS A STRING */
+	  	consumer_secret: '' // TODO: YOUR CONSUMER SECRET AS A STRING */
+  	};
 }
 
 exports.getAccessToken = function (appName) {
-	switch (appName) {
-		case 'MoodViz':
-			return { 
-		  		access_token_key: '', // TODO: YOUR ACCESS TOKEN KEY AS A STRING */
-	  			access_token_secret: '' // TODO:  YOUR ACCESS TOKEN SECRET AS A STRING */
-		  	};
-		default:
-			throw new Error('No access token was found for ' + appName);
-			return null;
-	};
+	return { 
+  		access_token_key: '', 	// TODO: YOUR ACCESS TOKEN KEY AS A STRING */
+		access_token_secret: '' // TODO:  YOUR ACCESS TOKEN SECRET AS A STRING */
+  	};
 }
 
 exports.getSentiment140AppId = function (appName) {
-	switch (appName) {
-		case 'MoodViz':
-			return ''; // TODO: YOUR SENTIMENT140 APP ID AS A STRING */
-		default:
-			throw new Error('No Sentiment140 App Id was found for ' + appName);
-			return null;
-	};
+	return ''; // TODO: YOUR SENTIMENT140 APP ID AS A STRING */
 }
