45cc75aa8eaa15a2c32b69cd1c8e9fe47ed7988c
Hadi Michael Salem <hadisalem7@gmail.com>
2013-06-30 12:30:51 +1000

    build a UI for config

--- public/config/config.js
+++ public/config/config.js
@@ -5,24 +5,46 @@
  *
  */
 
-$(function(){
-    // adding the script tag to the head as suggested before
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = window.location.origin + '/socket.io/socket.io.js';
+function submit() {
+    var action = {
+        doing: $('input[name="action.doing"]:checked').val(),
+        to: $('input[name="text"]').val().split(',')
+    }
+    
+    if (action.doing == '' || action.to == '') {alert('missing content');return;}
 
-    script.onload = function() { 
-        var socket = io.connect(window.location.origin); 
+    document.write("Doing stuff. Please wait...");
 
-        socket.on('connected', function (message) {
-            console.log(message); //display message
+    $.ajax({
+        url: window.location.origin + '/tunein',
+        error: function(error) {
+            console.log("Couldn't work out which channel to tune in to...")
+        },
+        success: function(channel) {
+            // adding the script tag to the head as suggested before
+            var head = document.getElementsByTagName('head')[0];
+            var script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = window.location.origin + '/socket.io/socket.io.js';
 
-            var keywords = ['new york', 'lunch'];
-            socket.emit('stream.track', keywords);
-        });
-    };
+            script.onload = function() { 
+                var socket = io.connect(window.location.origin); 
 
-    // fire
-    head.appendChild(script);
-});
+                socket.on('connected', function (message) {
+                    console.log(message); //display message
+
+                    //var keywords = ['new york', 'lunch'];
+                    socket.emit(action.doing, action.to);
+                });
+
+                socket.on(channel, function (response) {
+                    console.log(response);
+                    document.write(JSON.stringify(response));
+                });
+            };
+
+            // fire
+            head.appendChild(script);
+        },
+    });
+}
